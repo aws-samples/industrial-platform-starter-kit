80ae9f993ec7340f2f780360e39762d0c848acad
Takehiro Suzuki <tmgf.15.su@gmail.com>
2023-08-22 09:20:33 +0900

    fix workflow (#3)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,19 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { IndustialDataPlatformStack } from "../lib/industrial-data-platform-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("SnapshotTest", () => {
+  const app = new cdk.App();
+  const stack = new IndustialDataPlatformStack(app, "MyTestStack", {
+    thingName: "thing",
+    opcuaEndpointUri: "opc.tcp://localhost:4840",
+    provisionVirtualDevice: true,
+    env: {
+      account: "123456789012",
+      region: "ap-northeast-1",
+    },
+  });
+  const template = Template.fromStack(stack).toJSON();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(template).toMatchSnapshot();
 });
